3db08f8e3b142e43fd52d988bdcf83cf0565d50e
Sasanka <sasanka@Sasankas-MacBook-Air.local>
2026-06-09 22:26:41 +0530

    Subtree matching

--- Tree/SubtreeMatching.java
+++ Tree/SubtreeMatching.java
@@ -1,35 +1,59 @@
 public class SubtreeMatching {
-    class Node{
+
+    class Node {
         int data;
-        Node left,right;
-        Node(int data){
+        Node left, right;
+
+        Node(int data) {
             this.data = data;
             this.left = null;
             this.right = null;
         }
     }
+
     static int idx = -1;
-    Node buildTree(int nodes[]){
+
+    Node buildTree(int nodes[]) {
         idx++;
-        if (nodes[idx] == -1) {
+        if (idx >= nodes.length || nodes[idx] == -1) {
             return null;
         }
+
         Node newNode = new Node(nodes[idx]);
         newNode.left = buildTree(nodes);
         newNode.right = buildTree(nodes);
 
         return newNode;
     }
-    boolean isSubtree(Node root, Node subRoot){
-        if(root ==  null){
-            return false;
-        }
-        if(subRoot == null){
+
+    // MAIN FUNCTION
+    boolean isSubtree(Node root, Node subRoot) {
+
+        if (root == null) return false;
+        if (subRoot == null) return true;
+
+        if (isSameTree(root, subRoot)) {
             return true;
         }
 
+        return isSubtree(root.left, subRoot) ||
+               isSubtree(root.right, subRoot);
     }
+
+    // helper function
+    boolean isSameTree(Node p, Node q) {
+
+        if (p == null && q == null) return true;
+
+        if (p == null || q == null) return false;
+
+        if (p.data != q.data) return false;
+
+        return isSameTree(p.left, q.left) &&
+               isSameTree(p.right, q.right);
+    }
+
     public static void main(String[] args) {
-        
+        // You can test here
     }
 }

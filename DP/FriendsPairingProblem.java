cc39306e109b485d9e73e08c03e591fd2c99c3f0
Sasanka <sasanka@Sasankas-MacBook-Air.local>
2026-07-26 16:45:46 +0530

    Iterative  and dp tabulation

--- DP/FriendsPairingProblem.java
+++ DP/FriendsPairingProblem.java
@@ -24,4 +24,13 @@ public class FriendsPairingProblem {
          dp[n]= pair(n-1,dp) + (n-1) * pair(n-2,dp);
         return dp[n];
     }
+    private static int pairIterative(int n){
+        int dp[] = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3;i<n;i++){
+            dp[i] = dp[i-1] + (i-1) * dp[i-2];
+        }
+        return dp[n];
+    }
 }
